2692a0303dfb327b2c387acde5e4bf468c879215
MiniDigger <admin@minidigger.me>
2017-05-18 19:04:12 +0200

    finish wiki bot

--- tools/src/main/java/me/minidigger/customentities/tools/GenerateWiki.java
+++ tools/src/main/java/me/minidigger/customentities/tools/GenerateWiki.java
@@ -1,37 +1,52 @@
 package me.minidigger.customentities.tools;
 
-import java.io.File;
-import java.io.PrintWriter;
-import java.lang.reflect.Field;
-import java.util.Date;
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
-
+import com.artemis.Component;
+import me.minidigger.customentities.api.Documentation;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.lib.TextProgressMonitor;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
+import org.eclipse.jgit.util.FileUtils;
 import org.reflections.Reflections;
 
-import com.artemis.Component;
-
-import me.minidigger.customentities.api.Documentation;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.reflect.Field;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 public class GenerateWiki {
 
     private File outDir = new File("wiki");
 
-    private Set<String> sidebar = ConcurrentHashMap.newKeySet();
+    private Set<String> sidebar = new HashSet<>();
 
     private Git git;
+    private static String gitUser, gitPass;
 
     public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("You did not speficy user or pw for git, changes will not be pushed!");
+        } else {
+            gitUser = args[0];
+            gitPass = args[1];
+        }
+
         new GenerateWiki().start();
     }
 
     public void start() {
         System.out.println("Starting...");
-        if (outDir.exists())
-            outDir.delete();
+        if (outDir.exists()) {
+            System.out.println("Removing old wiki folder");
+            try {
+                FileUtils.delete(outDir, FileUtils.RECURSIVE);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
         System.out.println("Cloning wiki...");
         cloneWiki();
@@ -47,13 +62,15 @@ public class GenerateWiki {
 
         System.out.println("Cleanup...");
         git.close();
-        outDir.delete();
     }
 
     public void commitWiki() {
         try {
+            git.add().addFilepattern(".").call();
             git.commit().setMessage("Bot Update " + new Date().toString()).setAuthor("bot", "bot@minidigger.me").call();
-            // git.push().sert TODO PUSH
+            if (gitUser != null) {
+                git.push().setCredentialsProvider(new UsernamePasswordCredentialsProvider(gitUser, gitPass)).call();
+            }
         } catch (GitAPIException e) {
             e.printStackTrace();
         }
@@ -69,7 +86,7 @@ public class GenerateWiki {
     }
 
     public void generateComponents() {
-        new Reflections("me.minidigger").getSubTypesOf(Component.class).parallelStream().forEach(this::generateComponent);
+        new Reflections("me.minidigger").getSubTypesOf(Component.class).forEach(this::generateComponent);
     }
 
     public void generateSidebar() {
@@ -86,21 +103,27 @@ public class GenerateWiki {
     }
 
     public void generateComponent(Class<? extends Component> clazz) {
-        System.out.println("    Generate " + clazz.getSimpleName() + ".md");
-        Documentation documentation = clazz.getAnnotation(Documentation.class);
-        if (documentation == null)
-            return;
-
-        try (PrintWriter pw = new PrintWriter(new File(outDir, clazz.getSimpleName() + ".md"))) {
-            pw.println(documentation.val() + ":  ");
-            pw.println();
-            for (Field field : clazz.getDeclaredFields()) {
-                String doc = field.isAnnotationPresent(Documentation.class) ? ": " + field.getAnnotation(Documentation.class).val() : "";
-                pw.println("* " + field.getName() + ": " + field.getType().getName() + doc);
+        try {
+            File file = new File(outDir, clazz.getSimpleName() + ".md");
+            Documentation documentation = clazz.getAnnotation(Documentation.class);
+            if (documentation == null) {
+                return;
             }
+            System.out.println("    Generate " + file.getAbsolutePath());
 
-            sidebar.add("    * [[" + clazz.getSimpleName() + "|" + clazz.getSimpleName() + "]]");
-        } catch (Exception ex) {
+            try (PrintWriter pw = new PrintWriter(file)) {
+                pw.println(documentation.val() + ":  ");
+                pw.println();
+                for (Field field : clazz.getDeclaredFields()) {
+                    String doc = field.isAnnotationPresent(Documentation.class) ? ": " + field.getAnnotation(Documentation.class).val() : "";
+                    pw.println("* " + field.getName() + ": " + field.getType().getName() + doc);
+                }
+
+                sidebar.add("    * [[" + clazz.getSimpleName() + "|" + clazz.getSimpleName() + "]]");
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        } catch (Throwable ex) {
             ex.printStackTrace();
         }
     }
